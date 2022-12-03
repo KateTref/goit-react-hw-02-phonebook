296e5149d1759a0f7c8c692d1e56af19b7646e1b
KateTref <trefilovaekaterina408@gmail.com>
2022-12-03 22:42:23 +0200

    add filter finish

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import Filter from 'components/Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' }],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    ],
     filter: '',
     name: '',
     number: '',
@@ -22,8 +25,18 @@ class App extends Component {
     this.setState({ filter: evt.currentTarget.value });
   };
 
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter)
+    );
+  };
+
   render() {
     const { contacts, filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+
     return (
       <>
         <ContactForm onSubmit={this.addContact} />
@@ -31,7 +44,7 @@ class App extends Component {
           <>
             <h1>Contacts</h1>
             <Filter value={filter} onChange={this.changeFilter} />
-            <ContactList items={contacts} />
+            <ContactList items={visibleContacts} />
           </>
         )}
       </>
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Filter = ({ value, onChange }) => {
   return (
     <>
-      <label htmlFor="filter"></label>
+      <label htmlFor="filter">Find contacts by name</label>
       <input type="text" name="filter" value={value} onChange={onChange} />
     </>
   );
